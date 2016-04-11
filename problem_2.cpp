db6ab5124be4b2ac64e6ed7fe6a5181f9e047421
Rakesh Kumar <T_RakeshK3@verifone.com>
2016-04-11 07:34:34 +0100

    Solution to the second problem

---
+++ problem_2.cpp
@@ -0,0 +1,22 @@
+#include <cstdio>
+
+int main() {
+	int S = 0;
+	int A = 1;
+	int B = 2;
+	S += 2;
+
+	while (1) {
+		const int C = A + B;
+		A = B;
+		B = C;
+		if (C > 4000000)
+			break;
+		if (C % 2 == 0)
+			S += C;
+	}
+
+	printf("sum == [%d]\n", S);
+
+	return 0;
+}
