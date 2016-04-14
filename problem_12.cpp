5017e7483285bd1aaf171acfd8d6a09dc5928eaa
Rakesh Kumar <cpp.rakesh@gmail.com>
2016-04-14 22:44:09 +1000

    problem 12 solution

--- problem_12.cpp
+++ problem_12.cpp
@@ -1,39 +1,33 @@
 #include <cstdio>
-#include <vector>
 #include <ctime>
+#include <cmath>
 
-void divisors_numbers(const unsigned long long int& number) {
-	int count = 2;
-	printf("1 ");
-	for (int i = 2; i <= number / 2; ++i)
-		if (number % i == 0)
-			//++count;
-			printf("%d ", i);
-	printf("%d\n", number);
-	
-	//return count;
-}
+int divisor_count(const unsigned long long int& N) {
+  int counter = 0;
+  const int value = std::sqrt(static_cast<double>(N));
+  for (int i = 1; i <= value; ++i)
+    if (N % i == 0)
+      counter += 2;
 
-#if 0
-void triangular_number(const int& divisors) {
-	int iterator = 1;
-	unsigned long long int number = 0;
-	while (1) {
-		number += iterator;
-		if (divisors_numbers(number) > divisors) {
-			printf("number == [%llu]\n", number);
-			break;
-		}
-		++iterator;
-	}
+  if (value * value == N)
+    --counter;
+
+  return counter;
 }
-#endif
 
 int main() {
-	std::clock_t start = clock();
-	//triangular_number(400);
-	divisors_numbers(28);
-	divisors_numbers(56);
-	printf("Execution time == [%.2f]\n", (clock() - static_cast<double>(start)) / CLOCKS_PER_SEC);
-	return 0;
+  std::clock_t start = clock();
+  unsigned long long int number = 0;
+  int iter = 1;
+  while (1) {
+    number += iter;
+    ++iter;
+    if (divisor_count(number) > 500) {
+      printf("%llu\n", number);
+      break;
+    }
+  }
+
+  printf("Execution time == [%.2f]\n", (clock() - static_cast<double>(start)) / CLOCKS_PER_SEC);
+  return 0;
 }
