7be9f78d63c10209ce7381f07070cdef87db7145
cpp.rakesh <cpp.rakesh@gmail.com>
2016-04-29 04:38:29 +0100

    Factorial digit sum solution

---
+++ problem_20.cpp
@@ -0,0 +1,22 @@
+#include "common/Integer.h"
+#include <cstdio>
+#include <ctime>
+
+int main() {
+	std::clock_t start = clock();
+
+	Integer<int> number(100);
+	for (int i = 99; i >= 2; --i)
+		number *= i;
+
+	int sum = 0;
+	const std::vector<int> num = number.GetData();
+	for (std::size_t i = 0; i < num.size(); ++i)
+		sum += num[i];
+
+	printf("Sum of digits of 100! == [%d]\n", sum);
+	
+	printf("Execution time == [%.2f] seconds\n", (clock() - static_cast<double>(start)) / CLOCKS_PER_SEC);
+	
+	return 0;
+}
