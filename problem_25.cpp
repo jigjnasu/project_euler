3c1ba157b048ee9412465bfa164dea97f0cc040c
cpp.rakesh <cpp.rakesh@gmail.com>
2016-05-05 09:12:19 +0100

    Solution for 1000-digit Fibonacci number

---
+++ problem_25.cpp
@@ -0,0 +1,25 @@
+#include "common/Integer.h"
+#include <cstdio>
+#include <ctime>
+
+void fibonacci() {
+	Integer<int> A(1);
+	Integer<int> B(1);
+	Integer<int> C = A + B;
+	int index = 3;
+
+	while (C.GetData().size() < 1000) {
+		A = B;
+		B = C;
+		C = A + B;
+		++index;
+	}
+	printf("Index == [%d]\n", index);
+}
+
+int main() {
+	std::clock_t start = clock();
+	fibonacci();
+	printf("Execution time == [%.6f] seconds\n", (clock() - start) / static_cast<double>(CLOCKS_PER_SEC));
+	return 0;
+}
