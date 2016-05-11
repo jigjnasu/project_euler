12adc94f4f2d4aa50e45447cf555f4860a2b1029
cpp.rakesh <cpp.rakesh@gmail.com>
2016-05-11 09:43:48 +0100

    Let's work in next half

--- problem_38.cpp
+++ problem_38.cpp
@@ -1,20 +1,31 @@
 #include "common/QuickSort.h"
 #include <cstdio>
+#include <ctime>
 
+bool is_pandigital(std::string number) {
+	printf("number == [%s]\n", number.c_str());
+	if (number.size() != 9)
+		return false;
+
+	PROJECT_EULER::COMMON::QuickSort<char> sort;
+	sort.Sort(number);
+
+	printf("number == [%s]\n", number.c_str());
+
+	for (std::size_t i = 0; i < 10; ++i) {
+		if (number[i] != '0' + i + 1) {
+			printf("[%d] <---> [%d]\n", number[i], '0' + i + 1);
+			return false;
+		}
+	}
+
+	return true;
+}
 
 int main() {
-	PROJECT_EULER::COMMON::QuickSort<int> sort;
-	int A[] = {9, 8, 1, 3, 782, 2, 85, 2, 89};
-	std::vector<int> V(A, A + sizeof(A) / sizeof(A[0]));
-
-	for (std::size_t i = 0; i < V.size(); ++i)
-		printf("%d ", V[i]);
-	printf("\n");
-	
-	sort.Sort(V);
-	for (std::size_t i = 0; i < V.size(); ++i)
-		printf("%d ", V[i]);
-	printf("\n");
+	std::clock_t start = clock();
+	printf("%d\n", is_pandigital("876934512"));
+	printf("Execution time == [%.6f] seconds\n", (clock() - start) / static_cast<double>(CLOCKS_PER_SEC));
 	
 	return 0;
 }
