f8e0308e39c7a6e81f5088109ee47251eb67e24f
cpp.rakesh <cpp.rakesh@gmail.com>
2016-05-14 01:32:23 +1000

    Solution for Pentagon numbers

--- problem_44.cpp
+++ problem_44.cpp
@@ -1,27 +1,48 @@
+/*
+  This is solution for problem 29 (Distinct Powers) from ProjectEuler.net.
+  Author : Rakesh Kumar @ cpp.rakesh@gmail.com
+  Date : May 12th, 2016
+ */
+
 #include <cstdio>
-#include <vector>
 #include <ctime>
 #include <cmath>
 
-typedef u_long unsigned long long int;
-
-u_long pentagon_number(const int& N) {
-	return (N * ((3 * N) - 1)) >> 1;
+int pentagon_number(const int& N) {
+	return (N * ((3 * N) - 1)) / 2;
 }
 
-void sum_diff() {
-	u_long min_number = 0;
-	for (std::size_t i = 0; i < numbers.size() - 1; ++i) {
-		for (std::size_t j = i + 1; j < numbers.size(); ++j) {
-			
+bool is_pentagon_number(const unsigned long long int& number) {
+	const float N = (1 + std::sqrt(24 * number + 1)) / 6;
+	if (N - static_cast<int>(N) > 0.0)
+		return false;
+	
+	return true;
+}
+
+void find_min_difference() {
+	int p = 1;
+	while (1) {
+		const int A = pentagon_number(p);
+
+		for (int q = p - 1; q > 0; --q) {
+			const int B = pentagon_number(q);
+			if (is_pentagon_number(A + B) && is_pentagon_number(A - B)) {
+				printf("------------------------------------------------------\n");
+				printf("Sum == [%d] || Difference == [%d]\n", A + B, A - B);
+				printf("------------------------------------------------------\n");
+				return;
+			}
 		}
+		++p;
 	}
 }
 
-
 int main() {
 	std::clock_t start = clock();
-	sum_diff();
+	find_min_difference();
 	printf("Execution time == [%.6f] seconds\n", (clock() - start) / static_cast<double>(CLOCKS_PER_SEC));
+
+	
 	return 0;
 }
