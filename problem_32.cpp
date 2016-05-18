667738b5d4bf519677a6a3afa8999302f3571eaa
cpp.rakesh <cpp.rakesh@gmail.com>
2016-05-18 04:18:30 +0100

    solution for Pandigital Products

--- problem_32.cpp
+++ problem_32.cpp
@@ -1,53 +1,69 @@
 /*
   This is solution for problem 32 (Pandigital products) from ProjectEuler.net.
   Author : Rakesh Kumar @ cpp.rakesh@gmail.com
-  Date : May 12th, 2016
+  Date : May 18th, 2016
  */
 
-#include "common/QuickSort.h"
 #include <cstdio>
+#include <set>
 #include <ctime>
-#include <string>
-#include <sstream>
 
-bool is_pandigital(std::string number) {
-	PROJECT_EULER::COMMON::QuickSort<char> sort;
-	sort.Sort(number);
+bool is_pandigital(int A, int B, int C) {
+	int array[9] = {0};
 
-	if (number.size() != 9)
-		return false;
+	while (A) {
+		const int value = A % 10;
+		if (array[value - 1])
+			return false;
+		else
+			array[value - 1] = 1;
+		A /= 10;
+	}
+
+	while (B) {
+		const int value = B % 10;
+		if (array[value - 1])
+			return false;
+		else
+			array[value - 1] = 1;
+		B /= 10;
+	}
+
+	while (C) {
+		const int value = C % 10;
+		if (array[value - 1])
+			return false;
+		else
+			array[value - 1] = 1;
+		C /= 10;		
+	}
 
 	for (int i = 0; i < 9; ++i)
-		if (number[i] - '0' != i + 1)
+		if (array[i] == 0)
 			return false;
 
 	return true;
 }
 
-std::string number_to_string(const int& N) {
-	std::stringstream ss;
-	ss << N;
-	return ss.str();
-}
-
-void pandigital_products() {
-	int product_sum = 0;
-	for (int a = 1; a <= ; ++a) {
-		for (int b = 1; b <= ; ++b) {
-			const int c = a * b;
-			std::string number = number_to_string(a) + number_to_string(b) + number_to_string(c);
-			if (is_pandigital(number))
-				product_sum += c;
+void pandigital_product() {
+	std::set<int> products;
+	for (int i = 1; i < 100; ++i) {
+		for (int j = 100; j < 2000; ++j) {
+			if (is_pandigital(i, j, i * j))
+				products.insert(i * j);
 		}
 	}
+
+	int product_sum = 0;
+	for (std::set<int>::const_iterator it = products.begin(); it != products.end(); ++it)
+		product_sum += *it;
 
 	printf("Product sum of all pandigital numbers == [%d]\n", product_sum);
 }
 
 int main() {
 	std::clock_t start = clock();
-	pandigital_products();
+	pandigital_product();
 	printf("Execution time == [%.6f] seconds\n", (clock() - start) / static_cast<double>(CLOCKS_PER_SEC));
-
 	return 0;
 }
