ea7a045278d897f5fd86d07d62f97ad382891c8d
cpp-rakesh <cpp.rakesh@gmail.com>
2016-09-07 20:17:47 +1000

    Implemented Optimized Big Integer

--- problem_16.cpp
+++ problem_16.cpp
@@ -3,18 +3,18 @@
 #include <cstdio>
 
 int main() {
-	int N = 1000;
-	std::clock_t start = clock();
-	PROJECT_EULER::COMMON::Integer<int> A(2);
-	for (int i = 0; i < N - 1; ++i)
-		A = A * 2;
+    int N = 1000;
+    std::clock_t start = clock();
+    PROJECT_EULER::COMMON::Integer<int> A(2);
+    for (int i = 0; i < N - 1; ++i)
+	A = A * 2;
 
-	int sum = 0;
-	std::vector<int> number = A.GetData();
-	for (std::size_t i = 0; i < number.size(); ++i)
-		sum += number[i];
+    int sum = 0;
+    std::vector<int> number = A.GetData();
+    for (std::size_t i = 0; i < number.size(); ++i)
+	sum += number[i];
 
-	printf("sum == %d\n", sum);
+    printf("sum == %d\n", sum);
 	
-	printf("Execution time == [%.2f] seconds\n", (clock() - static_cast<double>(start)) / CLOCKS_PER_SEC);
+    printf("Execution time == [%.8f] seconds\n", (clock() - start) / static_cast<double>(CLOCKS_PER_SEC));
 }
--- problem_20.cpp
+++ problem_20.cpp
@@ -3,20 +3,20 @@
 #include <ctime>
 
 int main() {
-	std::clock_t start = clock();
+    std::clock_t start = clock();
 
-	PROJECT_EULER::COMMON::Integer<int> number(100);
-	for (int i = 99; i >= 2; --i)
-		number *= i;
+    PROJECT_EULER::COMMON::Integer<int> number(100);
+    for (int i = 99; i >= 2; --i)
+	number *= i;
 
-	int sum = 0;
-	const std::vector<int> num = number.GetData();
-	for (std::size_t i = 0; i < num.size(); ++i)
-		sum += num[i];
+    int sum = 0;
+    const std::vector<int> num = number.GetData();
+    for (std::size_t i = 0; i < num.size(); ++i)
+	sum += num[i];
 
-	printf("Sum of digits of 100! == [%d]\n", sum);
+    printf("Sum of digits of 100! == [%d]\n", sum);
 	
-	printf("Execution time == [%.2f] seconds\n", (clock() - static_cast<double>(start)) / CLOCKS_PER_SEC);
+    printf("Execution time == [%.8f] seconds\n", (clock() - start) / static_cast<double>(CLOCKS_PER_SEC));
 	
-	return 0;
+    return 0;
 }
