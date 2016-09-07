481e930b51f3a1af70eda1162a21ec245b40ed63
cpp-rakesh <cpp.rakesh@gmail.com>
2016-09-07 20:22:55 +1000

    Optimized updated Integer

--- problem_25.cpp
+++ problem_25.cpp
@@ -5,23 +5,23 @@
 namespace PC = PROJECT_EULER::COMMON;
 
 void fibonacci() {
-	PC::Integer<int> A(1);
-	PC::Integer<int> B(1);
-	PC::Integer<int> C = A + B;
-	int index = 3;
+    PC::Integer<int> A(1);
+    PC::Integer<int> B(1);
+    PC::Integer<int> C = A + B;
+    int index = 3;
 
-	while (C.GetData().size() < 1000) {
-		A = B;
-		B = C;
-		C = A + B;
-		++index;
-	}
-	printf("Index == [%d]\n", index);
+    while (C.GetData().size() < 1000) {
+	A = B;
+	B = C;
+	C = A + B;
+	++index;
+    }
+    printf("Index == [%d]\n", index);
 }
 
 int main() {
-	std::clock_t start = clock();
-	fibonacci();
-	printf("Execution time == [%.6f] seconds\n", (clock() - start) / static_cast<double>(CLOCKS_PER_SEC));
-	return 0;
+    std::clock_t start = clock();
+    fibonacci();
+    printf("Execution time == [%.8f] seconds\n", (clock() - start) / static_cast<double>(CLOCKS_PER_SEC));
+    return 0;
 }
--- problem_29.cpp
+++ problem_29.cpp
@@ -2,7 +2,7 @@
   This is solution for problem 29 (Distinct Powers) from ProjectEuler.net.
   Author : Rakesh Kumar @ cpp.rakesh@gmail.com
   Date : May 12th, 2016
- */
+*/
 
 #include "common/Integer.h"
 #include <cstdio>
@@ -11,25 +11,25 @@
 #include <cmath>
 
 void count_distinct_powers() {
-	std::set<std::string> distinct_numbers;
-	for (int a = 2; a <= 100; ++a) {
-		for (int b = 2; b <= 100; ++b) {
-			PROJECT_EULER::COMMON::Integer<int> number(a);
-			for (int i = 1; i < b; ++i) {
-				number *= a;
-			}
-			distinct_numbers.insert(number.GetStringData());
-		}
+    std::set<std::string> distinct_numbers;
+    for (int a = 2; a <= 100; ++a) {
+	for (int b = 2; b <= 100; ++b) {
+	    PROJECT_EULER::COMMON::Integer<int> number(a);
+	    for (int i = 1; i < b; ++i) {
+		number *= a;
+	    }
+	    distinct_numbers.insert(number.GetStringData());
 	}
+    }
 
-	printf("Distinct powers count == [%d]\n", distinct_numbers.size());
+    printf("Distinct powers count == [%lu]\n", distinct_numbers.size());
 }
 
 int main() {
-	std::clock_t start = clock();
-	count_distinct_powers();
-	printf("Execution time == [%.6f] seconds\n", (clock() - start) / static_cast<double>(CLOCKS_PER_SEC));
+    std::clock_t start = clock();
+    count_distinct_powers();
+    printf("Execution time == [%.8f] seconds\n", (clock() - start) / static_cast<double>(CLOCKS_PER_SEC));
 
-	return 0;
+    return 0;
 }
 
