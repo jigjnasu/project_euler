ef3d0172dad7aee7a29654341ae072edd492f0e4
cpp-rakesh <cpp.rakesh@gmail.com>
2016-09-07 20:14:33 +1000

    Changes regarding Big Integer template algorithm optimization

--- problem_10.cpp
+++ problem_10.cpp
@@ -3,24 +3,24 @@
 #include <ctime>
 
 bool is_prime(const unsigned long long int& N) {
-	for (int i = 2; i <= std::sqrt(N); ++i)
-		if (N % i == 0)
-			return false;
+    for (int i = 2; i <= std::sqrt(N); ++i)
+	if (N % i == 0)
+	    return false;
 
-	return true;
+    return true;
 }
 
 int main() {
-	std::clock_t start = clock();
-	PROJECT_EULER::COMMON::Integer<int> sum(0);
-	for (int i = 2; i < 2000000; ++i) {
-		if (is_prime(i)) {
-			sum += i;
-		}
+    std::clock_t start = clock();
+    PROJECT_EULER::COMMON::Integer<int> sum(0);
+    for (int i = 2; i < 2000000; ++i) {
+	if (is_prime(i)) {
+	    sum += i;
 	}
+    }
 
-	sum.Print();
+    sum.Print();
 
-	printf("Execution time == [%.2f] seconds\n", (clock() - static_cast<double>(start)) / CLOCKS_PER_SEC);
-	return 0;
+    printf("Execution time == [%.8f] seconds\n", (clock() - static_cast<double>(start)) / CLOCKS_PER_SEC);
+    return 0;
 }
--- problem_13.cpp
+++ problem_13.cpp
@@ -5,25 +5,25 @@
 #include <ctime>
 
 void large_sum(const std::string& file) {
-	PROJECT_EULER::COMMON::Integer<int> sum;
-	std::fstream ifs(file.c_str());
-	if (ifs.is_open()) {
-		std::string line;
-		while (std::getline(ifs, line)) {
-			std::reverse(line.begin(), line.end());
-			sum = sum + line;
-		}
-		sum.Print();
-		ifs.close();
-	} else {
-		printf("There is an error while %s opening the file\n", file.c_str());
+    PROJECT_EULER::COMMON::Integer<int> sum;
+    std::fstream ifs(file.c_str());
+    if (ifs.is_open()) {
+	std::string line;
+	while (std::getline(ifs, line)) {
+	    std::reverse(line.begin(), line.end());
+	    sum = sum + line;
 	}
+	sum.Print();
+	ifs.close();
+    } else {
+	printf("There is an error while %s opening the file\n", file.c_str());
+    }
 }
 
 int main() {
-	std::clock_t start = clock();
-	large_sum("data/large_sum.txt");
-	printf("Execution time == [%.2f] seconds\n", (clock() - static_cast<double>(start)) / CLOCKS_PER_SEC);
+    std::clock_t start = clock();
+    large_sum("data/large_sum.txt");
+    printf("Execution time == [%.8f] seconds\n", (clock() - start) / static_cast<double>(CLOCKS_PER_SEC));
 	
-	return 0;
+    return 0;
 }
