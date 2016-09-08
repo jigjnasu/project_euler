d916b409321a0806e0fd6fba12679f47cf997764
cpp-rakesh <cpp.rakesh@gmail.com>
2016-09-08 23:23:24 +1000

    Indendation

--- problem_48.cpp
+++ problem_48.cpp
@@ -2,7 +2,7 @@
   This is solution for problem 48 (Self Powers) from ProjectEuler.net.
   Author : Rakesh Kumar @ cpp.rakesh@gmail.com
   Date : May 13th, 2016
- */
+*/
 
 #include "common/Integer.h"
 #include <cstdio>
@@ -10,23 +10,23 @@
 
 
 void self_powers() {
-	PROJECT_EULER::COMMON::Integer<int> power_sum(0);
+    PROJECT_EULER::COMMON::Integer<int> power_sum(0);
 
-	for (int i = 1; i <= 1000; ++i) {
-		PROJECT_EULER::COMMON::Integer<int> number(1);
-		for (int j = 0; j < i; ++j) {
-			number *= i;
-		}
-		power_sum += number;
+    for (int i = 1; i <= 1000; ++i) {
+	PROJECT_EULER::COMMON::Integer<int> number(1);
+	for (int j = 0; j < i; ++j) {
+	    number *= i;
 	}
+	power_sum += number;
+    }
 
-	printf("Selft power sum == [%s]\n", power_sum.GetStringData().c_str());
+    printf("Selft power sum == [%s]\n", power_sum.GetStringData().c_str());
 }
 
 int main() {
-	std::clock_t start = clock();
-	self_powers();
-	printf("Execution time == [%.6f] seconds\n", (clock() - start) / static_cast<double>(CLOCKS_PER_SEC));
+    std::clock_t start = clock();
+    self_powers();
+    printf("Execution time == [%.6f] seconds\n", (clock() - start) / static_cast<double>(CLOCKS_PER_SEC));
 
-	return 0;
+    return 0;
 }
