18ad57deabb30a206380856ca3906bb9d554806a
cpp-rakesh <cpp.rakesh@gmail.com>
2016-09-14 02:50:07 +1000

    Integer modifications

--- problem_25.cpp
+++ problem_25.cpp
@@ -2,15 +2,13 @@
 #include <cstdio>
 #include <ctime>
 
-namespace PC = PROJECT_EULER::COMMON;
-
 void fibonacci() {
-    PC::Integer<int> A(1);
-    PC::Integer<int> B(1);
-    PC::Integer<int> C = A + B;
+    Integer A(1);
+    Integer B(1);
+    Integer C = A + B;
     int index = 3;
 
-    while (C.GetData().size() < 1000) {
+    while (C.GetReverse().size() < 1000) {
 	A = B;
 	B = C;
 	C = A + B;
--- problem_29.cpp
+++ problem_29.cpp
@@ -10,15 +10,26 @@
 #include <set>
 #include <cmath>
 
+Integer power(const int& x, const int& n) {
+    if (n == 1) {
+        return Integer(x);
+    } else {
+        if (n % 2 == 0) {
+            Integer value = power(x, n / 2);
+            return value * value;
+        } else {
+            Integer value = power(x, n / 2);
+            return value * value * x;
+        }
+    }
+}
+
 void count_distinct_powers() {
     std::set<std::string> distinct_numbers;
     for (int a = 2; a <= 100; ++a) {
 	for (int b = 2; b <= 100; ++b) {
-	    PROJECT_EULER::COMMON::Integer<int> number(a);
-	    for (int i = 1; i < b; ++i) {
-		number *= a;
-	    }
-	    distinct_numbers.insert(number.GetStringData());
+	    Integer number = power(a, b);
+	    distinct_numbers.insert(number.GetReverse());
 	}
     }
 
