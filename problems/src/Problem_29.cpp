6708e11872ff9e3ded294f0de01cf29e9fab3aaf
cpp-rakesh <cpp.rakesh@gmail.com>
2016-12-10 00:14:38 +1100

    Integer new name integration

--- problems/src/Problem_29.cpp
+++ problems/src/Problem_29.cpp
@@ -20,11 +20,9 @@ pp::Problem_29::~Problem_29() {}
 void pp::Problem_29::distinct_powers() const {
     utility::maths::Maths<utility::integer::Integer> maths;
     std::set<std::string> distinct_numbers;
-    for (int a = 2; a <= 100; ++a) {
-	for (int b = 2; b <= 100; ++b) {
-	    distinct_numbers.insert(maths.power(a, b).GetReverse());
-	}
-    }
+    for (int a = 2; a <= 100; ++a)
+	for (int b = 2; b <= 100; ++b)
+	    distinct_numbers.insert(maths.power(a, b).get_reverse());
     
     printf("Distinct powers count == [%lu]\n", distinct_numbers.size());    
 }
