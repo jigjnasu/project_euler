f0542ea3eca3d13f43efa035009547e6a2a05d6a
cpp-rakesh <cpp.rakesh@gmail.com>
2016-12-10 00:11:35 +1100

    Integer new name integration

--- problems/src/Problem_20.cpp
+++ problems/src/Problem_20.cpp
@@ -13,5 +13,5 @@ pp::Problem_20::~Problem_20() {}
 void pp::Problem_20::sum_of_factorial_digits(int n) const {
     utility::maths::Maths<utility::integer::Integer> maths;
     utility::string::String<int> str;
-    printf("Sum of digits of 100! == [%d]\n", str.sum_of_digits(maths.factorial(n).GetReverse()));
+    printf("Sum of digits of 100! == [%d]\n", str.sum_of_digits(maths.factorial(n).get_reverse()));
 }
