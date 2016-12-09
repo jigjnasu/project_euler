25b23e13cdd39691f4aa6ded890402d4441a8aea
cpp-rakesh <cpp.rakesh@gmail.com>
2016-12-10 00:13:49 +1100

    Integer new name integration

--- problems/src/Problem_16.cpp
+++ problems/src/Problem_16.cpp
@@ -11,12 +11,12 @@ pp::Problem_16::Problem_16() {}
 pp::Problem_16::~Problem_16() {}
 
 void pp::Problem_16::sum() const {
-    int y = 1000;
+    const int y = 1000;
     utility::integer::Integer x(2);
 
     utility::maths::Maths<utility::integer::Integer> maths;
 
     utility::string::String<int> str;
 
-    printf("Sum of 2 ^ 1000 digits == [%d]\n", str.sum_of_digits(maths.power(x, y).GetReverse()));
+    printf("Sum of 2 ^ 1000 digits == [%d]\n", str.sum_of_digits(maths.power(x, y).get_reverse()));
 }
