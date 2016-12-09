9909f7fdd2cac34b041e67e94fb664b516dd1f99
cpp-rakesh <cpp.rakesh@gmail.com>
2016-12-10 00:15:21 +1100

    Integer new name integration

--- problems/src/Problem_10.cpp
+++ problems/src/Problem_10.cpp
@@ -12,11 +12,9 @@ pp::Problem_10::~Problem_10() {}
 void pp::Problem_10::sum_of_primes() const {
     utility::maths::Maths<int> math;
     utility::integer::Integer sum(0);
-    for (int i = 2; i < 2000000; ++i) {
-	if (math.is_prime(i)) {
+    for (int i = 2; i < 2000000; ++i)
+	if (math.is_prime(i))
 	    sum += i;
-	}
-    }
 
-    sum.Print();
+    sum.print();
 }
